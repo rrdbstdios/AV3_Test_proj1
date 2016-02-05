d1a0cf6cc3a652ed8f6bdae5125e090dee03bbbe
Paul Lipp <paul.lipp@av3inc.com>
2016-02-04 20:48:02 -0500

    local commit updated module

--- SPlsWork/example_splus_mod.cs
+++ SPlsWork/example_splus_mod.cs
@@ -15,12 +15,28 @@ namespace UserModule_EXAMPLE_SPLUS_MOD
     {
         static CCriticalSection g_criticalSection = new CCriticalSection();
         
+        Crestron.Logos.SplusObjects.DigitalInput BUTTON_1;
+        Crestron.Logos.SplusObjects.DigitalInput BUTTON_2;
+        Crestron.Logos.SplusObjects.DigitalOutput BTNFB1;
+        Crestron.Logos.SplusObjects.DigitalOutput BTNFB2;
         
         public override void LogosSplusInitialize()
         {
             SocketInfo __socketinfo__ = new SocketInfo( 1, this );
             InitialParametersClass.ResolveHostName = __socketinfo__.ResolveHostName;
             _SplusNVRAM = new SplusNVRAM( this );
+            
+            BUTTON_1 = new Crestron.Logos.SplusObjects.DigitalInput( BUTTON_1__DigitalInput__, this );
+            m_DigitalInputList.Add( BUTTON_1__DigitalInput__, BUTTON_1 );
+            
+            BUTTON_2 = new Crestron.Logos.SplusObjects.DigitalInput( BUTTON_2__DigitalInput__, this );
+            m_DigitalInputList.Add( BUTTON_2__DigitalInput__, BUTTON_2 );
+            
+            BTNFB1 = new Crestron.Logos.SplusObjects.DigitalOutput( BTNFB1__DigitalOutput__, this );
+            m_DigitalOutputList.Add( BTNFB1__DigitalOutput__, BTNFB1 );
+            
+            BTNFB2 = new Crestron.Logos.SplusObjects.DigitalOutput( BTNFB2__DigitalOutput__, this );
+            m_DigitalOutputList.Add( BTNFB2__DigitalOutput__, BTNFB2 );
             
             
             
@@ -41,6 +57,10 @@ namespace UserModule_EXAMPLE_SPLUS_MOD
         
         
         
+        const uint BUTTON_1__DigitalInput__ = 0;
+        const uint BUTTON_2__DigitalInput__ = 1;
+        const uint BTNFB1__DigitalOutput__ = 0;
+        const uint BTNFB2__DigitalOutput__ = 1;
         
         [SplusStructAttribute(-1, true, false)]
         public class SplusNVRAM : SplusStructureBase
