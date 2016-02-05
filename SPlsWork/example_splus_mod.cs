7eea4260d816761487fde51f594011d51677a6eb
Paul Lipp <paul.lipp@av3inc.com>
2016-02-04 19:09:16 -0500

    Test commit of example crestron files

---
+++ SPlsWork/example_splus_mod.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using Crestron;
+using Crestron.Logos.SplusLibrary;
+using Crestron.Logos.SplusObjects;
+using Crestron.SimplSharp;
+
+namespace UserModule_EXAMPLE_SPLUS_MOD
+{
+    public class UserModuleClass_EXAMPLE_SPLUS_MOD : SplusObject
+    {
+        static CCriticalSection g_criticalSection = new CCriticalSection();
+        
+        
+        public override void LogosSplusInitialize()
+        {
+            SocketInfo __socketinfo__ = new SocketInfo( 1, this );
+            InitialParametersClass.ResolveHostName = __socketinfo__.ResolveHostName;
+            _SplusNVRAM = new SplusNVRAM( this );
+            
+            
+            
+            _SplusNVRAM.PopulateCustomAttributeList( true );
+            
+            NVRAM = _SplusNVRAM;
+            
+        }
+        
+        public override void LogosSimplSharpInitialize()
+        {
+            
+            
+        }
+        
+        public UserModuleClass_EXAMPLE_SPLUS_MOD ( string InstanceName, string ReferenceID, Crestron.Logos.SplusObjects.CrestronStringEncoding nEncodingType ) : base( InstanceName, ReferenceID, nEncodingType ) {}
+        
+        
+        
+        
+        
+        [SplusStructAttribute(-1, true, false)]
+        public class SplusNVRAM : SplusStructureBase
+        {
+        
+            public SplusNVRAM( SplusObject __caller__ ) : base( __caller__ ) {}
+            
+            
+        }
+        
+        SplusNVRAM _SplusNVRAM = null;
+        
+        public class __CEvent__ : CEvent
+        {
+            public __CEvent__() {}
+            public void Close() { base.Close(); }
+            public int Reset() { return base.Reset() ? 1 : 0; }
+            public int Set() { return base.Set() ? 1 : 0; }
+            public int Wait( int timeOutInMs ) { return base.Wait( timeOutInMs ) ? 1 : 0; }
+        }
+        public class __CMutex__ : CMutex
+        {
+            public __CMutex__() {}
+            public void Close() { base.Close(); }
+            public void ReleaseMutex() { base.ReleaseMutex(); }
+            public int WaitForMutex() { return base.WaitForMutex() ? 1 : 0; }
+        }
+         public int IsNull( object obj ){ return (obj == null) ? 1 : 0; }
+    }
+    
+    
+}
